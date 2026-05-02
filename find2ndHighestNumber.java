57c79884bc1293eea7a7b4d9a98e5a98ef5e9481
MUKUL SAINI <mukulsaini1843@gmail.com>
2026-05-02 06:57:46 +0530

    first commit

---
+++ find2ndHighestNumber.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class find2ndHighestNumber {
+    public static void main(String[] args) {
+        List<Integer> number = List.of(2,3,10,26,48,4, 94,6,3,2);
+        Optional<Integer> result = number.stream()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst();
+        result.ifPresent(System.out::println);
+
+    }
+}
