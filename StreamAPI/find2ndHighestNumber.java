b47396035bf3e997a19ef8f04fb1009a7a0f2bc0
MUKUL SAINI <mukulsaini1843@gmail.com>
2026-05-02 15:08:49 +0530

    without stream

--- StreamAPI/find2ndHighestNumber.java
+++ StreamAPI/find2ndHighestNumber.java
@@ -6,12 +6,28 @@ import java.util.Optional;
 
 public class find2ndHighestNumber {
     public static void main(String[] args) {
-        List<Integer> number = List.of(2,3,10,26,48,4, 94,6,3,2);
-        Optional<Integer> result = number.stream()
-                .sorted(Comparator.reverseOrder())
-                .skip(1)
-                .findFirst();
-        result.ifPresent(System.out::println);
+//        List<Integer> number = List.of(2,3,10,26,48,4, 94,6,3,2);
+//        Optional<Integer> result = number.stream()
+//                .sorted(Comparator.reverseOrder())
+//                .skip(1)
+//                .findFirst();
+//        result.ifPresent(System.out::println);
+       //output: 48
 
+        int[] arr = {2,3,10,26,48,4,101, 100, 87, 94,6,3,2};
+        int largest = 0;
+        int secondLargest = 0;
+        for (int element : arr) {
+            if(element > largest){
+                secondLargest = largest;
+                largest = element;
+            }else{
+                if(element > secondLargest && secondLargest != largest){
+                    secondLargest = element;
+                }
+            }
+        }
+        System.out.println(largest +"  "+secondLargest);
     }
+    //output: 101  100
 }
