ad911053f39b4c828624f03b0f7174d33918ea24
MUKUL SAINI <mukulsaini1843@gmail.com>
2026-05-12 11:51:31 +0530

    add some other string logic

--- StreamAPI/countRepeatedElement.java
+++ StreamAPI/countRepeatedElement.java
@@ -1,5 +1,6 @@
 package StreamAPI;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -11,8 +12,8 @@ public class countRepeatedElement {
         for (int j : arr) {
             element.put(j, element.getOrDefault(j, 0) + 1);
         }
-//        element.forEach((key, value)->
-//                System.out.println("element : "+key + " count : "+value));
+        element.forEach((key, value)->
+                System.out.println("element : "+key + " count : "+value));
 
         //count occurrence of each character in a string
         String str = "hello mukul , how are you doing today?";
@@ -24,16 +25,35 @@ public class countRepeatedElement {
                 result.put(c, result.getOrDefault(c, 0) + 1);
             }
         }
-//        result.forEach((key, value)->
-//                System.out.println("char: "+key+", count: "+ value));
+        result.forEach((key, value)->
+                System.out.println("char: "+key+", count: "+ value));
         // doing same with the stream api
         Map<Character, Long> output =  str.chars()
                 .filter(Character::isLetter)
+                .map(Character::toLowerCase)
                 .mapToObj(c->(char)c)
                 .collect(Collectors.groupingBy(
                         c -> c,
                         Collectors.counting()
                 ));
         System.out.println(output);
+
+        // find the count of single character in the string
+        String str1 = "sachinemukulmm";
+        long result1  = str1.toLowerCase()
+                . chars()
+                .mapToObj(c-> (char)c)
+                .filter(s-> s == 'm')
+                .count();
+        System.out.println("result1 : "+result1);
+
+        // if you have to find the count of character in from the list of strings
+        String[] strArray =  {"hello", "world", "java", "stream", "api"};
+        long result2 =  Arrays.stream(strArray)
+                .flatMapToInt(String::chars)
+                .mapToObj(c->(char)c)
+                .filter(c-> c == 'a')
+                .count();
+        System.out.println("result2 : "+result2 );
     }
 }
